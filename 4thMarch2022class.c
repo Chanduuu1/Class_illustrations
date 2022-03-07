ad6273b3682b86a27fb7433097bbf0571ed70135
Chanduuu1 <shekharkrishnan13@gmail.com>
2022-03-07 08:05:19 +0530

    Recursive function in c [factorial + A sp series]

---
+++ 4thMarch2022class.c
@@ -0,0 +1,57 @@
+/* Recursive function */
+// function calling the same function
+
+/* Flow of Control
+    return 4 * fact(3)
+                return 3 * fact(2)
+                            return 2 * fact(1)
+                                        return 1 * fact(0)
+                                                    this will return 1 because if condition
+    
+    Then last return will pass its value to uske upar wala return and so onn, but in computer sab ek jhatke me (simoultaneously) ho jaega.
+ */
+
+
+
+#include <stdio.h>
+long long int fact(int x){
+    int mul;
+    if (x<1){
+        return 1;
+
+    }
+    else{
+        return x * fact(x-1);
+    }
+
+}
+int main(){
+    printf("%lld", fact(5));
+}
+
+
+// printing the series 1 + 1! + 2! + 3! + ...
+#include <stdio.h>
+
+long long int series(int x){
+    if(x < 1){
+        return 1;
+        
+    }
+    else{
+        return x * series(x-1);
+    }
+
+}    
+int main(){
+    long long int sum = 0;
+    int n;
+    scanf("%d", &n);
+    
+    for(int i = n; i>0;i--){
+        sum += series(i);
+    }
+    
+    printf("%lld", sum);
+    return 0;
+}
