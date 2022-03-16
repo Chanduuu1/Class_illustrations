a609425bf53aa0f71862e238698916210671e914
Chanduuu1 <shekharkrishnan13@gmail.com>
2022-03-16 09:53:32 +0530

    nothing special intro to sruct and union

---
+++ 14thMarch2022Class.c
@@ -0,0 +1,11 @@
+/* Structure and union */
+// Collection of different data type/ item.
+// say i have these variables in a firm:- emp_id,emp_name,desig,sal
+/* struct stu_info{char[9] reg_no,
+                   char[20] stu_name,
+                   char[10] school,
+                   char[10] prog,
+                   float cgpa}; */
+
+// If you want to know the size that the structure occupies then
+// sizeof(stu_info);
